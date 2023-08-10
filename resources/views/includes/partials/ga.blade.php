0edd3aa09cc73cadefca7b09d2e40b3af88b7db3
Pankaj Kumar <managersingh1991@gmail.com>
2023-08-10 11:13:20 +0530

    First Commit

---
+++ resources/views/includes/partials/ga.blade.php
@@ -0,0 +1,6 @@
+@if(config('analytics.google-analytics') && config('analytics.google-analytics') !== 'UA-XXXXX-X')
+    {{-- Google Analytics: change UA-XXXXX-X to be your site's ID. --}}
+    <script>
+      
+    </script>
+@endif
